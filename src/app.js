d01849660fa02058327e405279b9904a88835aa7
Felipe FCRL <145389507+Lipe7k@users.noreply.github.com>
2026-03-01 22:55:10 -0300

    add folder views and ejs

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import 'dotenv/config';
+import path from 'path';
+import { fileURLToPath } from 'url';  
+
 
 const app = express();
 
+app.set("views", path.join(fileURLToPath(import.meta.url), "..", "views"));
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index', { title: 'Andro Tech', msg: 'Olá, mundo!' });
 })
 
 app.listen(process.env.PORT, () => {
